4340bd3a84ec26871a40f89494dbfbc5a5dcdc8a
Jason <jkuang5@buffalo.edu>
2020-02-21 00:49:43 -0500

    NASDAQ Tickers

--- hello.py
+++ hello.py
@@ -1,12 +1,3 @@
 import praw
-import csv
 
 
-input_file = csv.DictReader(open("nyse-listed_csv.csv"))
-for row in input_file:
-    writer = csv.DictWriter
-    if ("Common Stock" in row['Company Name']):
-        print(row)
-        
-        
-    
