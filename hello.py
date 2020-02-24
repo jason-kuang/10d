070ceca1bb3474fdcf7aa029db11a757b048c1ec
Jason <jkuang5@buffalo.edu>
2020-02-24 01:09:53 -0500

    Rudimental addition of DD filtering, and also privatized logins.

--- hello.py
+++ hello.py
@@ -1,3 +1,30 @@
 import praw
+import time
+import datetime
+import json
+
+
+with open("logindata.json") as json_data_file:
+    logindata = json.load(json_data_file)
+
+reddit = praw.Reddit(client_id=logindata["client_id"],client_secret=logindata["client_secret"],user_agent=logindata["user_agent"])
+tickersFile = open("tickers.txt", "r")
+tickerSet = []
+now = datetime.datetime.now()
+for line in tickersFile:
+    tickerSet.append(line.rstrip())
+topDay = reddit.subreddit('WallStreetBets').search('flair:'"DD", limit=500, syntax='lucene',time_filter='day')
+
+for posts in topDay:
+    for words in posts.title.split():
+        if words.startswith('$'):
+            print(words)
+
+
+    
+    
+
+
+
 
 
