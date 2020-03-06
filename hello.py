c80e706bb2b40cd1a259718537f997bf6c6f9860
jkuang5 <kuang316@gmail.com>
2020-03-06 17:47:57 -0500

    Updated sentiment tracking to display percentage of Bulls/Bears.
    Added a display showing the Most Bullish and Most Bearish.

--- hello.py
+++ hello.py
@@ -16,26 +16,52 @@ def setup():
     return reddit 
 
 
-def main():
-    bull = 0
-    bear = 0
-    neutral = 0
-    commentArray = []
-    reddit = setup()
-    for comment in reddit.subreddit('wallstreetbets').comments(limit=None):
-        commentArray.append(comment.body)
+def analyzeSentiments(commentArray):
+    largestBear = 0
+    largestBull = 0
+    notableBullandBear = ["Bear", "Bull"]
+    overall = [0, 0, 0, notableBullandBear]  # 1st number is Bull sentiment, 2nd is Bear, 3rd is Neutral.
     analyzer = SentimentIntensityAnalyzer()
     for sentence in commentArray:
         sentiment = analyzer.polarity_scores(sentence)
         if (sentiment["compound"] > .005) or (sentiment["pos"] > abs(sentiment["neg"])):
-            bull += 1
+            overall[0] += 1 #positive sentiment
+            if (sentiment["pos"] > largestBull and len(sentence) > 30):
+                largestBull = sentiment["pos"]
+                notableBullandBear[0] = sentence
         elif (sentiment["compound"] < -.005) or (abs(sentiment["neg"]) > sentiment["pos"]):
-            bear += 1
+            overall[1] += 1 #negative sentiment
+            if (sentiment["neg"] > largestBear and len(sentence) > 30):
+                largestBear = sentiment["neg"]
+                notableBullandBear[1] = sentence
         else:
-            neutral += 1
-    print("Bearish Sentiment: " + str(bear))
-    print("Bullish Sentiment: " + str(bull))
-    print("Neutral Sentiment: " + str(neutral))
+            overall[2] += 1 #neutral
+    return overall
+
+
+def extractComments(redditInstance):
+    commentArray = []
+    for comment in redditInstance.subreddit('wallstreetbets').comments(limit=None):
+        commentArray.append(comment.body)
+
+    return commentArray
+
+def resultPrinter(sentiment,total):
+    print("Bullish Sentiment: " + str(sentiment[0]))
+    print("Bearish Sentiment: " + str(sentiment[1]))
+    print("Neutral Sentiment: " + str(sentiment[2]))
+    print("Notable Bullish Comment: " + str(sentiment[3][0]))
+    print("Notable Bearish Comment: " + str(sentiment[3][1]))
+    print("Overall: " + '%.2f' % ((sentiment[0] / total) * 100) + "%" + " of the " + str(total) + " comments are bullish")
+
+def main():
+    redditInstance = setup()
+    commentArray = extractComments(redditInstance)
+    sentiment = analyzeSentiments(commentArray)
+    total = sentiment[0] + sentiment[1]
+    resultPrinter(sentiment,total)
+
+
 
 
 if __name__ == "__main__":
